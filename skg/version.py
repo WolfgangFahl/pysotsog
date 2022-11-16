2af75d6022d8addea9683b39c5a57c5de3c190d6
Wolfgang Fahl <wf@bitplan.com>
2022-11-16 13:38:09 +0100

    adds command line option

---
+++ skg/version.py
@@ -0,0 +1,18 @@
+'''
+Created on 2022-04-01
+
+@author: wf
+'''
+import skg
+
+class Version(object):
+    '''
+    Version handling for pysotsog
+    '''
+    name="pysotsog"
+    description='''python Library for Scholars to achieve "Standing on the shoulders of giants"'''
+    version=skg.__version__
+    date = '2022-11-16'
+    updated = '2022-11-16'
+
+        
