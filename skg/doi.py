dadd8ae8ae45d14866226828ef4a635e02d35c05
Wolfgang Fahl <wf@bitplan.com>
2022-12-09 16:52:06 +0100

    adds DOI bibtex lookup

--- skg/doi.py
+++ skg/doi.py
@@ -4,18 +4,27 @@ Created on 2022-11-22
 @author: wf
 '''
 import re
+import aiohttp
+
 class DOI:
     """
     Digital Object Identifier handling
     
     see e.g. https://www.wikidata.org/wiki/Property:P356
+    see https://www.doi.org/doi_handbook/2_Numbering.html#2.2
+    
     """
-    pattern=re.compile(r"(10\.[0-9]{4,}(?:\.[0-9]+)*(?:\/|%2F)(?:(?![\"&\'])\S)+)")
+    pattern=re.compile(r"((?P<directory_indicator>10)\.(?P<registrant_code>[0-9]{4,})(?:\.[0-9]+)*(?:\/|%2F)(?:(?![\"&\'])\S)+)")
   
-    def __init__(self):
+    def __init__(self,doi:str):
         """
         a DOI
         """
+        self.doi=doi
+        match=re.match(DOI.pattern,doi)
+        self.ok=bool(match)
+        if self.ok:
+            self.registrant_code=match.group("registrant_code")
         
     @classmethod
     def isDOI(cls,doi:str):
@@ -34,5 +43,25 @@ class DOI:
             return ok
         if not isinstance(doi,str):
             return False
-        match=re.match(cls.pattern,doi)
-        return bool(match)
+        doi_obj=DOI(doi)
+        return doi_obj.ok
+    
+    async def fetch_text(self,url,headers):
+        """
+        fetch text for the given url with the given headers
+        """
+        async with aiohttp.ClientSession(headers=headers) as session:
+            async with session.get(url) as response:
+                return await response.text()
+    
+    async def doi2bibTex(self):
+        """
+        get the bibtex result for my doi
+        """
+        url=f"https://doi.org/{self.doi}"
+        headers= {
+            'Accept': 'application/x-bibtex; charset=utf-8'
+        }
+        return await self.fetch_text(url,headers)                 
+                    
+       
--- tests/test_doi.py
+++ tests/test_doi.py
@@ -7,6 +7,7 @@ from tests.basetest import Basetest
 from skg.doi import DOI
 from skg.dblp import Dblp
 from dataclasses import dataclass
+from unittest import IsolatedAsyncioTestCase
 
 @dataclass
 class DOIExample:
@@ -18,15 +19,17 @@ class TestDOI(Basetest):
     test DOI access
     """
     
-    
     def testDOICheck(self):
         """
         check DOI regex
         """
-        doi_examples=[DOIExample("10.1007/11581116_19",True),DOIExample("xyz",False)]
+        doi_examples=[
+            DOIExample("10.1007/11581116_19",True),
+            DOIExample("10.1109/TBDATA.2022.3224749",True),
+            DOIExample("xyz",False)]
         for doi_example in doi_examples:
             self.assertTrue(DOI.isDOI(doi_example.doi)==doi_example.isDoi,doi_example.doi)
-                            
+            
     def test_dblp_dois(self):
         """
         test dblp dois
@@ -37,3 +40,23 @@ class TestDOI(Basetest):
         paper_records=dblp.get_paper_records("CEUR Workshop Proceedings","publishedin",limit=limit,debug=debug)
         for paper_record in paper_records:
             print(paper_record)
+           
+class TestDOILookup(IsolatedAsyncioTestCase): 
+    """
+    test DOI lookup
+    """
+    async def testDOILookup(self):
+        """
+        test DOI lookup 
+        """
+        debug=False
+        dois=["10.1109/TBDATA.2022.3224749"]
+        for doi in dois:
+            doi_obj=DOI(doi)
+            result=await doi_obj.doi2bibTex()
+            if debug:
+                print(result)
+            self.assertTrue(result.startswith("@article{Li_2022,"))
+            pass
+                            
+   
